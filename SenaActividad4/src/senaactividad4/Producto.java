f8bc4e5f407d6707750c90a6c5ae4c4f7aaca509
Julian Parra <julianf.parrap@ecci.edu.co>
2023-07-26 22:52:56 -0500

    coding...50%

---
+++ SenaActividad4/src/senaactividad4/Producto.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senaactividad4;
+
+/**
+ *
+ * @author Julian
+ */
+public class Producto {
+
+    private String nombre;
+    private int identificacion;
+    private int numeroSerie;
+    private double precio;
+    private String proveedor;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, int identificacion, int numeroSerie, double precio, String proveedor,int cantidad) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.numeroSerie = numeroSerie;
+        this.precio = precio;
+        this.proveedor = proveedor;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public int getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(int numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", identificacion=" + identificacion + ", numeroSerie=" + numeroSerie + ", precio=" + precio + ", Proveedor=" + proveedor + '}';
+    }
+
+}
